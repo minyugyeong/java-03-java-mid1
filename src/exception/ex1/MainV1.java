0d977d9454cc95d7270c35b125f123da63b79cf2
minyugyeong <alsdbrud123@naver.com>
2024-07-16 17:20:43 +0900

    섹션 9 예외 처리1(이론) - 예외 처리가 필요한 이유3(반환 값으로 예외 처리)

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -1,13 +1,13 @@
 package exception.ex1;
 
-import exception.ex0.NetworkServiceV0;
-
 import java.util.Scanner;
 
 public class MainV1 {
 
     public static void main(String[] args) {
-        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        //NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        //NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
+        NetworkServiceV1_3 networkService = new NetworkServiceV1_3();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
