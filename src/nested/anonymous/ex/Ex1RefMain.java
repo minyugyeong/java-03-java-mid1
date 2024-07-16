7cd6ff3ca92b09419b91529375f45a99e6ee0a64
minyugyeong <alsdbrud123@naver.com>
2024-07-16 15:14:13 +0900

    섹션 8 중첩클래스, 내부 클래스2 - 익명 클래스 활용2

---
+++ src/nested/anonymous/ex/Ex1RefMain.java
@@ -0,0 +1,43 @@
+package nested.anonymous.ex;
+
+import java.util.Random;
+
+public class Ex1RefMain {
+    
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+        
+        //코드 조각 시작
+        process.run();
+        //코드 조각 종류
+
+        System.out.println("프로그램 종료");
+    }
+
+    static class Sum implements Process {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <= 3; i++) {
+                System.out.println("i = " + i);
+            }
+        }
+    }
+
+    static class Dice implements Process {
+
+        @Override
+        public void run() {
+            int randomValue = new Random().nextInt(6) + 1;
+            System.out.println("주사위 = " + randomValue);
+        }
+    }
+
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+        Sum sum = new Sum();
+        System.out.println("Hello 실행");
+        hello(dice);
+        hello(sum);
+    }
+}
